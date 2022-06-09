71803698e0c41d8ee40197be1f61edae02241785
LyuboslavVeliev <lub.veliev@abv.bg>
2022-06-09 21:09:41 +0300

    Is user athorized function added

--- pages/identity.js
+++ pages/identity.js
@@ -2,6 +2,16 @@ function saveAccessToken(token) {
     localStorage.setItem('auth_token', token);
 }
 
+function isAuthorized() {
+    let authTokenValue = localStorage.getItem('auth_token');
+
+    if (authTokenValue != null && authTokenValue != 'undefined') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 export default {
     saveAccessToken,
     isAuthorized
