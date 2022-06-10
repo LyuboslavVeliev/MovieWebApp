7918f8672cf18a3a3231c835d9690e916d68afd2
LyuboslavVeliev <lub.veliev@abv.bg>
2022-06-10 12:57:02 +0300

    Adjust navbar if authorized

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import login from './pages/login.js';
 import register from './pages/register.js';
+import identity from './pages/identity.js';
 
 let pages = {
     register: register,
@@ -8,10 +9,14 @@ let pages = {
 
 let navBarElement = document.querySelector('.navbar-nav');
 
+adjustNavBarIfAuthorized();
+
 navBarElement.addEventListener('click', (e) => {
     e.preventDefault();
 
     hidePages();
+
+    adjustNavBarIfAuthorized();
 
     let page = e.target.getAttribute('data-link');
 
@@ -20,4 +25,18 @@ navBarElement.addEventListener('click', (e) => {
 
 function hidePages() {
     Object.values(pages).forEach(page => page.hidePage());
+}
+
+function adjustNavBarIfAuthorized() {
+    if (identity.isAuthorized()) {
+        let navBarLinks = document.querySelectorAll('.nav-link');
+
+        for (const link of navBarLinks) {
+            if (link.getAttribute('data-link') == 'login' || link.getAttribute('data-link') == 'register') {
+                link.classList.add('hidden');
+            } else if(link.getAttribute('data-link') == 'logout') {
+                link.classList.remove('hidden');
+            }
+        }
+    }
 }
